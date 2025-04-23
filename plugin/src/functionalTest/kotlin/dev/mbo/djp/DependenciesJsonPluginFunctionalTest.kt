7af97aaff63d8a964865f760ca964ff25f354e97
Manuel Bogner <manuel.bogner@openresearch.com>
2025-04-23 23:27:55 +0200

    improve test

--- plugin/src/functionalTest/kotlin/dev/mbo/djp/DependenciesJsonPluginFunctionalTest.kt
+++ plugin/src/functionalTest/kotlin/dev/mbo/djp/DependenciesJsonPluginFunctionalTest.kt
@@ -1,12 +1,13 @@
 package dev.mbo.djp
 
 import com.sun.net.httpserver.HttpServer
+import org.gradle.testkit.runner.BuildResult
 import org.gradle.testkit.runner.GradleRunner
 import org.gradle.testkit.runner.TaskOutcome
 import org.junit.jupiter.api.Test
 import java.io.File
 import java.net.InetSocketAddress
-import java.nio.file.Path
+import java.util.*
 import kotlin.test.assertEquals
 import kotlin.test.assertTrue
 
@@ -21,53 +22,76 @@ pluginManagement {
     }
 }
 rootProject.name = "test-project"
-            """
+"""
+        private const val BUILD_GRADLE_KTS = """
+plugins {
+    java
+    id("dev.mbo.djp.dependencies-json") version "1.0.0-SNAPSHOT"
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("com.google.guava:guava:31.1-jre")
+}
+"""
     }
 
     @Test
     fun `plugin writes dependencies to JSON file`() {
-        val testProjectPath: Path = kotlin.io.path.createTempDirectory(prefix = "test-plugin")
-        val testProjectDir = testProjectPath.toFile()
-        File(testProjectDir, "settings.gradle.kts").writeText(SETTINGS_GRADLE_KTS)
-        File(testProjectDir, "build.gradle.kts").writeText(
-            """
-                plugins {
-                    java
-                    id("dev.mbo.djp.dependencies-json") version "1.0.0-SNAPSHOT"
-                }
-                
-                repositories {
-                    mavenCentral()
-                }
-                
-                dependencies {
-                    implementation("com.google.guava:guava:31.1-jre")
-                }
-                """.trimIndent()
-        )
+        val testProjectDir = prepareProject()
 
-        // Run the plugin task
-        val result = GradleRunner.create()
-            .withProjectDir(testProjectDir)
-            .withPluginClasspath()
-            .withArguments("dependencies-json", "--info")
-            .forwardOutput()
-            .build()
-
-        assertTrue(result.output.contains("Wrote dependency JSON"), "Expected log message not found!")
-
-        val outputFile = File(testProjectDir, "build/dependencies.json")
+        val result = runTask("dependencies-json", testProjectDir)
 
         // Assertions
         assertEquals(TaskOutcome.SUCCESS, result.task(":dependencies-json")?.outcome)
+        val outputFile = File(testProjectDir, "build/dependencies.json")
         assertTrue(outputFile.exists(), "Expected file ${outputFile.path} to exist")
-        val outContent = outputFile.readText()
-        assertTrue(outContent.contains("guava"), "Expected output to contain 'guava'")
+        assertTrue(outputFile.readText().contains("guava"), "Expected output to contain 'guava'")
+        // also check log message to exist
+        assertTrue(result.output.contains("Wrote dependency JSON"), "Expected log message not found!")
     }
 
     @Test
     fun `plugin uploads JSON file to http server`() {
-        val server = HttpServer.create(InetSocketAddress("localhost", 8089), 0).apply {
+        val server = startUploadServer()
+        try {
+            val testProjectDir = prepareProject(
+                additionalConfig = """
+                    dependenciesJson {
+                        postUrl.set("http://localhost:${server.address.port}/upload")
+                    }
+                """.trimIndent()
+            )
+
+            val result = runTask("dependencies-json-upload", testProjectDir)
+            assertEquals(TaskOutcome.SUCCESS, result.task(":dependencies-json-upload")?.outcome)
+        } finally {
+            server.stop(0)
+        }
+    }
+
+    fun prepareProject(
+        name: String = UUID.randomUUID().toString(),
+        additionalConfig: String = ""
+    ): File {
+        val testProjectPath = kotlin.io.path.createTempDirectory(prefix = name)
+        val testProjectDir = testProjectPath.toFile()
+        File(testProjectDir, "settings.gradle.kts").writeText(SETTINGS_GRADLE_KTS)
+        File(testProjectDir, "build.gradle.kts").writeText(
+            """
+            $BUILD_GRADLE_KTS
+            
+            $additionalConfig
+        """.trimIndent()
+        )
+        return testProjectDir
+    }
+
+    private fun startUploadServer(port: Int = 0): HttpServer {
+        return HttpServer.create(InetSocketAddress("localhost", port), 0).apply {
             createContext("/upload") { exchange ->
                 exchange.sendResponseHeaders(200, 0)
                 exchange.responseBody.close()
@@ -75,42 +99,14 @@ rootProject.name = "test-project"
             executor = null
             start()
         }
-        try {
-            val testProjectPath: Path = kotlin.io.path.createTempDirectory(prefix = "test-plugin2")
-            val testProjectDir = testProjectPath.toFile()
-            File(testProjectDir, "settings.gradle.kts").writeText(SETTINGS_GRADLE_KTS)
-            File(testProjectDir, "build.gradle.kts").writeText(
-                """
-                plugins {
-                    java
-                    id("dev.mbo.djp.dependencies-json") version "1.0.0-SNAPSHOT"
-                }
-                
-                repositories {
-                    mavenCentral()
-                }
-                
-                dependencies {
-                    implementation("com.google.guava:guava:31.1-jre")
-                }
-                
-                dependenciesJson {
-                    postUrl.set("http://localhost:8089/upload")
-                }
-                """.trimIndent()
-            )
+    }
 
-            // Run the plugin task
-            val result = GradleRunner.create()
-                .withProjectDir(testProjectDir)
-                .withPluginClasspath()
-                .withArguments("dependencies-json-upload", "--info")
-                .forwardOutput()
-                .build()
-
-            assertEquals(TaskOutcome.SUCCESS, result.task(":dependencies-json-upload")?.outcome)
-        } finally {
-            server.stop(0)
-        }
+    private fun runTask(task: String, projectDir: File): BuildResult {
+        return GradleRunner.create()
+            .withProjectDir(projectDir)
+            .withPluginClasspath()
+            .withArguments(task, "--info")
+            .forwardOutput()
+            .build()
     }
 }
