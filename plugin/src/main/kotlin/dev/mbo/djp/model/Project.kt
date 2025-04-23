527d72dfb5f152aa41636058c1aff5c0c80e2932
Manuel Bogner <manuel.bogner@openresearch.com>
2025-04-24 01:31:30 +0200

    let's use a string ts

--- plugin/src/main/kotlin/dev/mbo/djp/model/Project.kt
+++ plugin/src/main/kotlin/dev/mbo/djp/model/Project.kt
@@ -1,10 +1,11 @@
 package dev.mbo.djp.model
 
 import java.time.Instant
+import java.time.format.DateTimeFormatter
 
 data class ProjectDto(
     val name: String,
     val version: String,
     val dependencies: Map<String, List<Map<String, String>>>,
-    val timestamp: Instant = Instant.now(),
+    val timestamp: String = DateTimeFormatter.ISO_INSTANT.format(Instant.now())
 )
