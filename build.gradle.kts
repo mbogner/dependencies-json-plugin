f3708d7eddf9c1b00cb5e6503bdb3b2e2016024e
Manuel Bogner <manuel.bogner@openresearch.com>
2025-04-23 23:14:43 +0200

    fix java version and some warnings in build

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,10 @@
 plugins {
+    // https://mvnrepository.com/artifact/org.jetbrains.kotlin/kotlin-gradle-plugin
+    // Downgraded:
+    // The `embedded-kotlin` and `kotlin-dsl` plugins rely on features of Kotlin `2.0.21`
+    // that might work differently than in the requested version `2.1.20`.
+    kotlin("jvm") version "2.0.21"
+
     // https://plugins.gradle.org/plugin/net.researchgate.release
     id("net.researchgate.release") version "3.1.0"
 }
@@ -8,9 +14,18 @@ allprojects {
         mavenLocal()
         mavenCentral()
     }
+
+    plugins.withType<JavaPlugin>().configureEach {
+        extensions.configure<JavaPluginExtension> {
+            toolchain.languageVersion.set(JavaLanguageVersion.of(21))
+        }
+    }
+
+    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+        compilerOptions.jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_21)
+    }
 }
 
-// Hook into plugin publishing inside plugin module
 tasks.named("afterReleaseBuild") {
     dependsOn(":plugin:publishPlugins")
 }
